9bbc2c50ca796b41eae1aa82db846d1a9410a96e
Henry Pap <onkelhoy@gmail.com>
2018-03-15 13:29:32 +0100

    importing data code done

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+# loading the dependencies
 import csv
 import numpy as np
 
+# load the data
 filename = 'output.csv'
 raw_data = open(filename, 'rt')
 reader = csv.reader(raw_data, delimiter=',')
 x = list(reader)
 data = np.array(x).astype('int')
 print(data.shape)
+
+# split the data to train and test 70/30 split 
